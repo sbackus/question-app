b838c71184a9a7589ec99f0b7091d77f6c904ebf
Sam Backus <Samuel.Backus@gmail.com>
2019-05-01 10:11:21 -0400

    use react on the frontend

--- app/javascript/packs/main.jsx
+++ app/javascript/packs/main.jsx
@@ -1,4 +1,4 @@
-// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// Run this example by adding <%= javascript_pack_tag 'main' %> to the head of your layout file,
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
@@ -6,21 +6,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const Main = props => (
+  <div>Greetings {props.name}!</div>
 )
 
-Hello.defaultProps = {
+Main.defaultProps = {
   name: 'David'
 }
 
-Hello.propTypes = {
+Main.propTypes = {
   name: PropTypes.string
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Main name="React" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
