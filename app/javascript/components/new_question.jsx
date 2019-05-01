33cb5cf0759c28c62da14491e8b826de8f885048
Sam Backus <Samuel.Backus@gmail.com>
2019-05-01 12:23:35 -0400

    create new questions

--- app/javascript/components/new_question.jsx
+++ app/javascript/components/new_question.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 
 class NewQuestion extends React.Component {
+  constructor (props) {
+    super(props)
+    this.textInputRef = React.createRef()
+  }
+
+  handleClick () {
+    var text = this.textInputRef.current.value
+    console.log('The text value is ' + text)
+    fetch('/questions', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ text: text })
+    })
+  }
+
   render () {
     return (
-      <>
-        <p> new question form </p>
-      </>
+      <form>
+        <input ref={this.textInputRef} placeholder='what is your question' />
+        <button onClick={() => this.handleClick()}>Submit</button>
+      </form>
     )
   }
 }
